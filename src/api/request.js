c3c8c07ac08c74f1a25659a5fa05fb2d9be14dc2
SweetLollipop <137623637@qq.com>
2022-07-07 22:25:25 +0800

    017尚品汇-nprogress进度条的使用

--- src/api/request.js
+++ src/api/request.js
@@ -1,5 +1,9 @@
 //对于axios进行二次封装
 import axios from "axios";
+//引入进度条
+import nprogress from "nprogress";//其中的方法start：进度条开始 done：进度条结束
+//引入进度条样式
+import "nprogress/nprogress.css";
 
 //1、利用axios对象的方法create,去创建一个axios实例
 //2、request就是axios，只不过稍微配置一下
@@ -14,13 +18,17 @@ const requests = axios.create({
 //请求拦截器：在发请求之前，请求拦截器可以检测到，可以在请求发出去之前做一些事情
 requests.interceptors.request.use((config)=>{
     //config：配置对象，对象里面有一个属性很重要，headers请求头
+    //进度条开始动
+    nprogress.start();
     return config;
 });
 
 //响应拦截器
-requests.interceptors.response.use((use)=>{
+requests.interceptors.response.use((res)=>{
     //成功的回调函数：服务器响应数据回来之后，响应拦截器可以检测到，可以做一些事情
-    return resizeBy.data;
+    //进度条结束
+    nprogress.done();
+    return res.data;
 },(error)=>{
     //服务器响应失败的回调函数,例如：终止Promise链
     return Promise.reject(new Error('faile'));
