e1254bd1013332037ece871cd7d057cb930235ef
SweetLollipop <137623637@qq.com>
2022-07-11 22:27:13 +0800

    019尚品汇-动态展示三级联动数据

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 //三级联动组件----全局组件
-import TypeNav from '@/pages/Home/TypeNav';
+import TypeNav from '@/components/TypeNav';
 //第一个参数：全局组件的名字 第二个参数：哪一个组件
 Vue.component(TypeNav.name, TypeNav);
 //引入路由
@@ -10,9 +10,7 @@ import router from '@/router';
 import store from '@/store';
 
 Vue.config.productionTip = false
-//测试
-import {reqCategoryList} from '@/api';
-reqCategoryList();
+
 new Vue({
   render: h => h(App),
   //注册路由：底下的写法KV一致省略V【router首字母小写】
--- src/store/home/index.js
+++ src/store/home/index.js
@@ -1,7 +1,23 @@
+import { reqCategoryList } from "@/api";
 //home模块的小仓库
-const state = {a: 1};
-const mutations = {};
-const actions = {};
+const state = {
+    //state中的数据的数据类型是根据接口返回值初始化的
+    categoryList: [],
+};
+const mutations = {
+    CATEGORYLSIT(state, categoryList) {
+        state.categoryList = categoryList;
+    }
+};
+const actions = {
+    //通过API里面的接口函数，向服务器发请求，获取服务器的数据
+    async categoryList({ commit }) {
+        let result = await reqCategoryList();
+        if (result.code === 200) {
+            commit('CATEGORYLSIT', result.data);
+        }
+    }
+};
 const getters = {};
 export default {
     state,
