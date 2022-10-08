93a9998b7e6670149c65dd9c1002c338a96e405c
SweetLollipop <137623637@qq.com>
2022-10-08 22:26:37 +0800

    067尚品汇-放大镜结束

--- src/pages/Detail/Zoom/Zoom.vue
+++ src/pages/Detail/Zoom/Zoom.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="spec-preview">
     <img :src="imgObj.imgUrl" />
-    <div class="event"></div>
+    <div class="event" @mousemove="handler"></div>
     <div class="big">
-      <img :src="imgObj.imgUrl" />
+      <img :src="imgObj.imgUrl" ref="bigImg"/>
     </div>
-    <div class="mask"></div>
+    <!-- 遮罩层:青色的 -->
+    <div class="mask" ref="mask"></div>
   </div>
 </template>
 
@@ -29,6 +30,26 @@ export default {
     this.$bus.$on('getIndex',(index)=> {
       this.i = index;
     })
+  },
+  methods: {
+    handler(event) {
+      let mask = this.$refs.mask;
+      let bigImg = this.$refs.bigImg;
+      //计算间距
+      let left = event.offsetX - mask.offsetWidth / 2;
+      let top = event.offsetY - mask.offsetHeight / 2;
+      //约束范围
+      if(left < 0) left = 0;
+      if(left > mask.offsetWidth) left = mask.offsetWidth;
+      if(top < 0) top = 0;
+      if(top > mask.offsetHeight) top = mask.offsetHeight;
+      //修改元素的left和top的属性值
+      mask.style.left = left + 'px';
+      mask.style.top = top + 'px';
+      //修改大图的left和top(bigImg图的img是普通图的2倍且展示的方位与普通图方位相反所以 *-2)
+      bigImg.style.left = -2 * left + 'px';
+      bigImg.style.top = -2 * top + 'px';
+    }
   }
 };
 </script>
