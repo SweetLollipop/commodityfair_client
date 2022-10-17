dfd03e7eb3b338bad66dd49b349b19d2807b182f
SweetLollipop <137623637@qq.com>
2022-10-18 00:53:25 +0800

    075尚品汇-购物车动态展示数据

--- src/store/shopcart/index.js
+++ src/store/shopcart/index.js
@@ -1,15 +1,27 @@
 import { reqCartList } from "@/api";
 
-const state = {};
-const mutations = {};
-const actions = {
-    //获取购物车列表数据
-    async getCartList({comments}) {
-        let result = await reqCartList();
-        console.log(result);
+const state = {
+    cartList:[],
+};
+const mutations = {
+    GETCARTLIST(state,cartList){
+        state.cartList = cartList;
     }
 };
-const getters = {};
+const actions = {
+    //获取购物车列表数据
+    async getCartList({commit}) {
+        let result = await reqCartList();
+        if(result.code==200){
+            commit("GETCARTLIST",result.data);
+        }
+    }
+};
+const getters = {
+    cartList(state) {
+        return state.cartList[0]||{};
+    }
+};
 export default {
     state,
     mutations,
