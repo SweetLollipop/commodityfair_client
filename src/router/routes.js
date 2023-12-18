54dfbad0c58b2cd1cd1990cc08bac399c3b54f38
SweetLollipop <137623637@qq.com>
2023-12-18 22:03:36 +0800

    102尚品汇-个人中心二级路由搭建

--- src/router/routes.js
+++ src/router/routes.js
@@ -9,7 +9,30 @@ import ShopCart from '@/pages/ShopCart'
 import Trade from '@/pages/Trade';
 import Pay from '@/pages/Pay';
 import PaySuccess from '@/pages/PaySuccess';
+import Center from '@/pages/Center';
+import MyOrder from '@/pages/Center/myOrder'
+import GroupOrder from '@/pages/Center/groupOrder'
 export default  [
+    {
+        path: "/center",
+        component: Center,
+        meta: {show: true},
+        //二级路由组件
+        children: [
+            {
+              path: 'myorder',
+              component: MyOrder,
+            },
+            {
+                path: 'grouporder',
+                component: GroupOrder,
+            },
+            {
+                path: '/center',
+                redirect: '/center/myorder',
+            }
+        ]
+    },
     {
         path: "/paysuccess",
         component: PaySuccess,
