03f502bc57bd18e04bfdb4a69a6f0048c06c997f
SweetLollipop <137623637@qq.com>
2024-01-04 23:41:29 +0800

    103尚品汇-我的订单

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -19,8 +19,8 @@
           </p>
         </div>
         <div class="typeList">
-          <a href="###">我的订单</a>
-          <a @click="$router.push('/shopcart')">我的购物车</a>
+          <a @click="$router.push('/center/myorder')" style="cursor:pointer">我的订单</a>
+          <a @click="$router.push('/shopcart')" style="cursor:pointer">我的购物车</a>
           <a href="###">我的尚品汇</a>
           <a href="###">尚品汇会员</a>
           <a href="###">企业采购</a>
